492043db541a195ea1e853ac8b5aadc3a17b60df
Bassim <bassimsadik@hotmail.com>
2017-03-15 15:52:20 +0000

    generated shortURL and redirect to longURL

--- express_server.js
+++ express_server.js
@@ -2,6 +2,7 @@ var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080; // default port 8080
 
+//creates random 6 character string
 function generateRandomString() {
   var text = "";
     var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
@@ -16,6 +17,7 @@ generateRandomString();
 
 app.set("view engine", "ejs")
 
+//Added body parser
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -23,7 +25,7 @@ var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-
+console.log(urlDatabase);
 app.get("/", (req, res) => {
   res.end("Hello!");
 });
@@ -42,6 +44,7 @@ app.get("/urls", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
+  let website = req.params.new
   res.render("urls_new");
 });
 
@@ -51,11 +54,21 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
-
+//generates a shortURL key with associated input website.
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // debug statement to see POST parameters
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  let key = generateRandomString(); //.push(urlDatabase[req.params.id]);   // debug statement to see POST parameters
+  urlDatabase[key] = req.body.longURL;
+  console.log(urlDatabase);
+  res.redirect('http://localhost:8080/urls/'+key);         // Respond with 'Ok' (we will replace this)
 });
+
+//redirects shortURL to full website.
+app.get("/u/:shortURL", (req, res) => {
+
+  res.redirect(urlDatabase[req.params.shortURL]);
+
+});
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
