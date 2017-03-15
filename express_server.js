954a4745eac7d257338924339a1bd1196d7f5466
Bassim <bassimsadik@hotmail.com>
2017-03-15 18:10:45 +0000

    eslint check and revisions

--- express_server.js
+++ express_server.js
@@ -1,31 +1,31 @@
-var express = require("express");
-var app = express();
-var PORT = process.env.PORT || 8080; // default port 8080
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8080;
 
-//creates random 6 character string
-function generateRandomString() {
-  var text = "";
-    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-    for( var i=0; i < 6; i++ ){
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
-    };
-    console.log(text);
-    return text;
-}
-generateRandomString();
-
-app.set("view engine", "ejs")
+//added view engine to use ejs.
+app.set("view engine", "ejs");
 
 //Added body parser
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
-var urlDatabase = {
+//creates random 6 character string
+function generateRandomString() {
+  let text = "";
+  let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for( let i = 0; i < 6; i++ ){
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+}
+generateRandomString();
+
+const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-console.log(urlDatabase);
+
 app.get("/", (req, res) => {
   res.end("Hello!");
 });
@@ -43,33 +43,32 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+//Gives page to enter URL to shorten.
 app.get("/urls/new", (req, res) => {
-  let website = req.params.new
+  let website = req.params.new;
   res.render("urls_new");
 });
 
+// renders webpage for given id.
 app.get("/urls/:id", (req, res) => {
-  let longURL = urlDatabase[req.params.id]
+  let longURL = urlDatabase[req.params.id];
   let templateVars = { shortURL: req.params.id, longURL: longURL};
   res.render("urls_show", templateVars);
 });
 
 //generates a shortURL key with associated input website.
 app.post("/urls", (req, res) => {
-  let key = generateRandomString(); //.push(urlDatabase[req.params.id]);   // debug statement to see POST parameters
+  let key = generateRandomString();
   urlDatabase[key] = req.body.longURL;
   console.log(urlDatabase);
-  res.redirect('http://localhost:8080/urls/'+key);         // Respond with 'Ok' (we will replace this)
+  res.redirect('http://localhost:8080/urls/' + key);
 });
 
 //redirects shortURL to full website.
 app.get("/u/:shortURL", (req, res) => {
-
   res.redirect(urlDatabase[req.params.shortURL]);
-
 });
 
-
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+  console.log(`App listening on port ${PORT}!`);
 });
