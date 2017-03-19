deeb576ff5dde90c45be5b57e1cf0f0eeba77d6b
Bassim <bassimsadik@hotmail.com>
2017-03-19 06:31:44 +0000

    finished user-specs and adjusted for tech specs

--- express_server.js
+++ express_server.js
@@ -24,20 +24,28 @@ function generateRandomString() {
 }
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "b2xVn2": {
+    shortURL: "b2xVn2",
+    longURL: "http://www.lighthouselabs.ca",
+    userID: "userRandomID"
+  },
+  "9sm5xK": {
+    shortURL: "9sm5xK",
+    longURL: "http://www.google.com",
+    userID: "user2RandomID"
+  }
 };
 
 const users = {
   "userRandomID": {
     id: "userRandomID",
     email: "user@example.com",
-    password: "purple-monkey-dinosaur"
+    password: "1"
   },
  "user2RandomID": {
     id: "user2RandomID",
     email: "user2@example.com",
-    password: "dishwasher-funk"
+    password: "2"
   }
 }
 /**
@@ -47,7 +55,12 @@ const users = {
 *  redirect -> /login
  */
 app.get("/", (req, res) => {
-  res.end("Hello!");
+  let userId = req.cookies.userId;
+  if (userId) {
+    res.redirect("/urls");
+  } else {
+    res.redirect("/login");
+  }
 });
 
 app.get("/urls.json", (req, res) => {
@@ -58,10 +71,26 @@ app.get("/hello", (req, res) => {
   res.end("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+function findUrlsByUserId (userID) {
+  let userURLS = {};
+  for (let shortURL in urlDatabase) {
+    if (urlDatabase[shortURL].userID === userID) {
+      userURLS[shortURL] = urlDatabase[shortURL].longURL;
+    }
+  }
+  return userURLS;
+}
+
 app.get("/urls", (req, res) => {
-  let userId = req.cookies.userId;
-  let templateVars = {user: users[userId], urls: urlDatabase };
-  res.render("urls_index", templateVars);
+  let user = users[req.cookies.userId];
+  if (user) {
+    let myUrls = findUrlsByUserId(user.id);
+    console.log(myUrls);
+    let templateVars = {user: user, myUrls: myUrls };
+    res.render("urls_index", templateVars);
+  } else {
+    res.status(401).render("error");
+  }
 });
 
 //Gives page to enter URL to shorten.
@@ -69,31 +98,65 @@ app.get("/urls/new", (req, res) => {
   let userId = req.cookies.userId;
   if (userId) {
   let templateVars = {user: users[userId]};
-  // let website = req.params.new;
   res.render("urls_new", templateVars);
   } else {
-    res.redirect("/login");
+    res.status(401).render("error");
   }
 });
 
 // renders webpage for given id.
 app.get("/urls/:id", (req, res) => {
   let userId = req.cookies.userId;
-  let longURL = urlDatabase[req.params.id];
-  let templateVars = {user: users[userId], shortURL: req.params.id, longURL: longURL};
-  res.render("urls_show", templateVars);
+    if (!urlDatabase[req.params.id]) {
+      res.status(404).send("Error 404: Short URL does not exist!");
+  } else if (!userId) {
+      res.status(401).render('error');
+  } else if (userId !== urlDatabase[req.params.id].userID) {
+      res.status(403).send("Error 403 - Forbidden: You do not have access to this page.")
+  } else {
+      let longURL = urlDatabase[req.params.id].longURL;
+      let templateVars = {user: users[userId], shortURL: (req.params.id), longURL: longURL};
+      res.render("urls_show", templateVars);
+  }
+});
+
+app.post("/urls/:id", (req, res) => {
+  let userId = req.cookies.userId;
+  if (!urlDatabase[req.params.id]) {
+      res.status(404).send("Error 404: Short URL does not exist!");
+  } else if (!userId) {
+      res.status(401).render('error');
+  } else if (userId !== urlDatabase[req.params.id].userID) {
+      res.status(403).send("Error 403 - Forbidden: You do not have access to this page.")
+  } else {
+  urlDatabase[req.params.id].longURL = req.body.longURL;
+  res.redirect('/urls/'+req.params.id);
+  }
 });
 
 //generates a shortURL key with associated input website.
 app.post("/urls", (req, res) => {
-  let key = generateRandomString();
-  urlDatabase[key] = req.body.longURL;
-  res.redirect('/urls/' + key);
+  let user = users[req.cookies.userId]
+  if (user) {
+    let key = generateRandomString();
+    urlDatabase[key] = {
+      "shortURL": key,
+      "longURL": req.body.longURL,
+      "userID": user.id
+    };
+    res.redirect('/urls/' + key);
+  } else {
+    res.status(401).render('error');
+  }
 });
 
 //redirects shortURL to full website.
-app.get("/u/:shortURL", (req, res) => {
-  res.redirect(urlDatabase[req.params.shortURL]);
+app.get("/u/:id", (req, res) => {
+  if (!urlDatabase[req.params.id]){
+    res.status(404).send("Error 404 - ShortURL does not exist");
+  } else {
+      res.redirect(urlDatabase[req.params.id].longURL);
+  }
 });
 
 app.post("/urls/:id/delete", (req, res) => {
@@ -101,13 +164,14 @@ app.post("/urls/:id/delete", (req, res) => {
   res.redirect('/urls');
 });
 
-app.post("/urls/:id", (req, res) => {
-  urlDatabase[req.params.id] = req.body.longURL;
-  res.redirect('/urls');
-});
 
 app.get ("/login", (req, res) => {
-  res.render("login");
+  let userId = req.cookies.userId;
+  if (userId) {
+    res.redirect("/");
+  } else {
+    res.render("login");
+  }
 });
 
 app.post("/login", (req, res) => {
@@ -116,8 +180,8 @@ app.post("/login", (req, res) => {
     if (users[userId].email === req.body.email) {
       user = users[userId];
       break;
-      }
     }
+  }
   if (user) {
     if (user.password === req.body.password) {
       res.cookie("userId", user.id);
@@ -125,7 +189,7 @@ app.post("/login", (req, res) => {
       return;
     }
   }
-  res.status(403).send('Bad credentials');
+  res.status(401).send("401 Error - Bad Credentials");
 });
 
 app.post("/logout", (req, res) => {
@@ -134,18 +198,24 @@ app.post("/logout", (req, res) => {
 });
 
 app.get("/register", (req, res) => {
-  res.render("register");
+  let userId = req.cookies.userId;
+    if (userId) {
+      res.redirect("/");
+    } else {
+      res.render("register");
+    }
 });
 
-app.post("/register", (req, res) => {
-  function findExistingEmail(email) {
-    for (let userId in users) {
-      if (users[userId].email === email) {
-        return true;
-      }
+function findExistingEmail(email) {
+  for (let userId in users) {
+    if (users[userId].email === email) {
+      return true;
     }
-    return null;
   }
+  return null;
+}
+
+app.post("/register", (req, res) => {
   if (!req.body.email || !req.body.password) {
     res.status(400);
     res.send("Error: email or password not entered");
